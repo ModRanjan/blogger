212c7d64c4317f8db0ea832a335efaae2ebb684e
ModRanjan <ranjan.m@simformsolutions.com>
2023-08-04 13:15:12 +0530

    added-constants-for-navItems-&-blogPosts

---
+++ constants/NavItems.ts
@@ -0,0 +1,16 @@
+export type NavItemType = {
+  name: string;
+  url: string;
+};
+
+export const NavItems: NavItemType[] = [
+  {
+    name: 'Home',
+    url: '/',
+  },
+
+  {
+    name: 'Positions',
+    url: '/positions',
+  },
+];
